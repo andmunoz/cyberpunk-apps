6d5e219c83afcfc941f984d4442aab6c06753013
Andrés Muñoz O <andmunoz@moit.cl>
2023-12-01 11:05:56 -0300

    add cybertools project to repo

--- cybertools/cybertools/asgi.py
+++ cybertools/cybertools/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for firebase project.
+ASGI config for cybertools project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firebase.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cybertools.settings')
 
 application = get_asgi_application()
