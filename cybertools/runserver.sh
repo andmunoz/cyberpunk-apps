cb428306776421268026635fe210d3372ae9109d
Andrés Muñoz O <andmunoz@moit.cl>
2024-01-12 14:04:17 -0300

    feat: add shell script with virtualenv

--- cybertools/runserver.sh
+++ cybertools/runserver.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-python manage.py runserver
+nombre_del_entorno="cybertools"
+service_port=8000
+
+echo "### Creando entorno virtual $nombre_del_entorno..."
+if [ -d "venv" ]; then
+    echo "El entorno virtual $nombre_del_entorno ya existe!"
+else
+    python -m venv $nombre_del_entorno
+fi
+
+echo "### Activando entorno virtual $nombre_del_entorno..."
+source $nombre_del_entorno/bin/activate
+
+echo "### Instalando dependencias..."
+pip install -r requirements.txt
+
+echo "### Actualizando base de datos..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "### Iniciando el servicio en http://localhost:$service_port/..."
+python manage.py runserver $service_port
